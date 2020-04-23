38221aafe64e130f8fad3d94f7200960ee2e1d00
Kratu Desai <kratudesai@Kratus-MacBook-Pro.local>
2020-04-23 10:30:14 -0500

    change to run minutes in every seconds

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,13 +18,12 @@ clickCopy = (event) => {
     $(el).remove();
 }
 
-// current time
-let time = moment();
-
-
 // set interval for everysecond
 setInterval(myTimer, 1000);
 function myTimer() {
+    // current time
+    let time = moment();
+
     // adding Year Month Date Hour Minute in one code
     let code = `${time.format('YY')}${time.format('MM')}${time.format('DD')}${time.format('HH')}${time.format('mm')}`
 
@@ -39,16 +38,17 @@ function myTimer() {
 
     // adding Year Quarter Week log code2
     let code3 = `${time.format('YY')}0${time.format('Q')}${time.format('ww')}0${time.format('E')}${time.format('HH')}${time.format('mm')}`;
-
+    
     // adding value in section two
     $("#section3").text(code3).addClass('btn btn-warning btn-lg').click((event) => { clickCopy(event); });
-
+    
     // if you don't want to use moment.js then use this one (vanila javascript)
-    let d = new Date();
-    let dd = `${d.getFullYear().toString()[2]}${d.getFullYear().toString()[3]}${!d.getMonth().toString()[1] ? 0 + "" + (d.getMonth() + 1) : (d.getMonth() + 1)}${!d.getDate().toString()[1] ? 0 + "" + d.getDate() : d.getDate()}${!d.getHours().toString()[1] ? 0 + "" + d.getHours() : d.getHours()}${!d.getMinutes().toString()[1] ? 0 + "" + d.getMinutes() : d.getMinutes()}${!d.getSeconds().toString()[1] ? 0 + "" + d.getSeconds() : d.getSeconds()}`;
+    // let d = new Date();
+    // let dd = `${d.getFullYear().toString()[2]}${d.getFullYear().toString()[3]}${!d.getMonth().toString()[1] ? 0 + "" + (d.getMonth() + 1) : (d.getMonth() + 1)}${!d.getDate().toString()[1] ? 0 + "" + d.getDate() : d.getDate()}${!d.getHours().toString()[1] ? 0 + "" + d.getHours() : d.getHours()}${!d.getMinutes().toString()[1] ? 0 + "" + d.getMinutes() : d.getMinutes()}${!d.getSeconds().toString()[1] ? 0 + "" + d.getSeconds() : d.getSeconds()}`;
+    let code4 = `${time.format('YY')}0${time.format('Q')}${time.format('ww')}0${time.format('E')}${time.format('HH')}${time.format('mm')}${time.format('ss')}`;
 
     // adding value in section one
-    $("#section4").text(dd).addClass('btn btn-light btn-lg btn-block').click((event) => { clickCopy(event); });
+    $("#section4").text(code4).addClass('btn btn-light btn-lg btn-block').click((event) => { clickCopy(event); });
 }
 
 
