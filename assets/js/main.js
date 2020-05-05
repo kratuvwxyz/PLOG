c46241749999446e231737fcfd09dc0b6de9f7bb
Kratu Desai <kratudesai@Kratus-MacBook-Pro.local>
2020-05-05 10:01:22 -0500

    click to save password is ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,22 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 });
 
+// password generator
+let password = [];
+password = password.join();
+
 /* Window Width for Resizing fonts */
 // window width
-let wW = $(document).width();console.log(wW);
+let wW = $(document).width(); console.log(wW);
 
 function wWf() {
     wW = $(document).width();
     console.log(wW + "new");
     myTimer();
-    
+
 }
 // window on resize
-window.onresize = wWf;console.log(wW);
+window.onresize = wWf; console.log(wW);
 
 /* click and copy function */
 clickCopy = (event) => {
@@ -38,7 +42,7 @@ function myTimer() {
     let code = `${time.format('YY')}${time.format('MM')}${time.format('DD')}${time.format('HH')}${time.format('mm')}`
 
     // adding value in section one
-    $("#section1").text(code).addClass('btn-sect-1').css('font-size', (wW/1.0625) + "%")/* .hover(function(){$(this).css('font-size', (wW/1.0625) + "%")},function(){$(this).css('font-size', (wW/1.075) + "%")}).active(function(){$(this).css('font-size', (wW/1.0725) + "%")}) */.click(function(){$(this).css('font-size', (wW/1.0725) + "%")}).click((event) => { clickCopy(event); });
+    $("#section1").text(code).addClass('btn-sect-1').css('font-size', (wW / 1.0625) + "%")/* .hover(function(){$(this).css('font-size', (wW/1.0625) + "%")},function(){$(this).css('font-size', (wW/1.075) + "%")}).active(function(){$(this).css('font-size', (wW/1.0725) + "%")}) */.click(function () { $(this).css('font-size', (wW / 1.0725) + "%") }).click((event) => { clickCopy(event); });
 
     // adding Year Quarter Week log code2
     let code2 = `${time.format('YY')}0${time.format('Q')}${time.format('ww')}`;
@@ -48,10 +52,10 @@ function myTimer() {
 
     // adding Year Quarter Week log code2
     let code3 = `${time.format('YY')}0${time.format('Q')}${time.format('ww')}0${time.format('E')}${time.format('HH')}${time.format('mm')}`;
-    
+
     // adding value in section three
     $("#section3").text(code3).addClass('btn btn-warning btn-lg btn-fix-width').click((event) => { clickCopy(event); });
-    
+
     // if you don't want to use moment.js then use this one (vanila javascript)
     // let d = new Date();
     // let dd = `${d.getFullYear().toString()[2]}${d.getFullYear().toString()[3]}${!d.getMonth().toString()[1] ? 0 + "" + (d.getMonth() + 1) : (d.getMonth() + 1)}${!d.getDate().toString()[1] ? 0 + "" + d.getDate() : d.getDate()}${!d.getHours().toString()[1] ? 0 + "" + d.getHours() : d.getHours()}${!d.getMinutes().toString()[1] ? 0 + "" + d.getMinutes() : d.getMinutes()}${!d.getSeconds().toString()[1] ? 0 + "" + d.getSeconds() : d.getSeconds()}`;
@@ -59,12 +63,30 @@ function myTimer() {
 
     // adding value in section four
     $("#section4").text(code4).addClass('btn btn-light btn-lg btn-fix-width btn-block').click((event) => { clickCopy(event); });
-    
+
     // adding time from 1970 till now in microsecond
     let code5 = Date.now();
     // adding value in section five
     $("#section5").text(code5).addClass('btn btn-danger btn-lg btn-fix-width btn-block').click((event) => { clickCopy(event); });
+
+    // password
+    $("#section6").text(password.join("")).addClass('btn btn-dark btn-lg btn-block').click((event) => { clickCopy(event); });
 }
 
+// set interval for every minute
+setInterval(oneMinute, 60000);
+function oneMinute() {
+    // password generator
+    password = [];
+    let a = "$#@abcdefghkmnpqrstuvwxyz23456789";
+    let b = a.toUpperCase();
+    let c = a.concat(b).split("");
+    for(let i = 0; i < 20; i++){
+        let d = Math.floor(Math.random()*c.length);
+        password.push(c[d]);
+    }
+    
+}
+oneMinute();
 
 
